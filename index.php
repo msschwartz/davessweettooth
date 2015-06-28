5168cd8ef09a54adc95971ad85bfe3b114757748
Michael Schwartz <msschwartz@outlook.com>
2015-06-28 12:18:32 -0400

    Update index.php

--- index.php
+++ index.php
@@ -65,7 +65,7 @@
       <div class="title">Pouch Collection</div>
       <div class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
       <div class="price"><span class="dollars"><?php echo $product_config->pouch_collection_price; ?></span> <span class="shipping">free shipping</span></div>
-      <div class="buy"><a href="#" class="btn cg-EZaddtocart" oid="<?php echo $pouch->pouch_collection_order_id; ?>">Buy</a></div>
+      <div class="buy"><a href="#" class="btn cg-EZaddtocart" oid="<?php echo $product_config->pouch_collection_order_id; ?>">Buy</a></div>
     </div>
     
     
@@ -88,7 +88,7 @@
       <div class="title">Mini Jar Collection</div>
       <div class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
       <div class="price"><span class="dollars"><?php echo $product_config->mini_jar_collection_price; ?></span> <span class="shipping">free shipping</span></div>
-      <div class="buy"><a href="#" class="btn cg-EZaddtocart" oid="<?php echo $pouch->mini_jar_collection_order_id; ?>">Buy</a></div>
+      <div class="buy"><a href="#" class="btn cg-EZaddtocart" oid="<?php echo $product_config->mini_jar_collection_order_id; ?>">Buy</a></div>
     </div>
     
     
