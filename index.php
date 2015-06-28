bcc5e9203c314da15961133f5c355ab8c6386697
Michael Schwartz <mschwartz@levelsetsolutions.com>
2015-06-28 15:58:07 -0400

    Cache busting

--- index.php
+++ index.php
@@ -18,9 +18,10 @@
   <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&signed_in=true"></script>
   
   <!-- site css and js -->
-  <script type="text/javascript" src="/js/main.js?v=2"></script>
-  <link rel="stylesheet" href="/css/fonts.css?v=2">
-  <link rel="stylesheet" href="/css/main.css?v=2">
+  <?php $version = '1.1'; ?>
+  <script type="text/javascript" src="/js/main.js?v=<?php echo $version; ?>"></script>
+  <link rel="stylesheet" href="/css/fonts.css?v=<?php echo $version; ?>">
+  <link rel="stylesheet" href="/css/main.css?v=<?php echo $version; ?>">
 </head>
 <body id="davessweettooth" ontouchstart="">
   
